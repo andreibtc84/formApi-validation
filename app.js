ded71b0bf481a0e7293720ebb3c72cdc6a95a844
Andrei Btc <andreibtc@gmail.com>
2022-02-17 15:32:13 +0000

    Adds pasword validation functions

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const email = document.querySelector(".email");
 const password = document.querySelector(".password");
 const submit = document.querySelector(".submit");
 
+// Validation FUNCTIONS:
+// At least 8 chars
+
+const atLeast8 = (str) => (str.length >= 8 ? true : false);
+
+// Has at least one lowercase and uppercase letter, number, and a special character
+
+const charCheck = (str) => {
+  return (
+    /[a-z]/.test(str) &&
+    /[A-Z]/.test(str) &&
+    /\W|_/g.test(str) &&
+    /[0-9]/g.test(str)
+  );
+};
+
 const submitForm = (e) => {
   e.preventDefault();
   fetch(`https://reqres.in/api/users`, {
